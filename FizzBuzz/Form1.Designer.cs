fdb8d4b47308f17eb0dfcbe8d881ff04e455fdcb
Gustavo S <Gustavo S@DESKTOP-RJU5PVS>
2023-10-15 12:52:18 -0600

    Agregar archivos de proyecto.

---
+++ FizzBuzz/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace FizzBuzz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            this.btnjugar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Chiller", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(162, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FizzBuzz";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(50, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(403, 144);
+            this.label2.TabIndex = 1;
+            this.label2.Text = resources.GetString("label2.Text");
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Location = new System.Drawing.Point(171, 358);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(100, 23);
+            this.txtnumero.TabIndex = 2;
+            // 
+            // btnjugar
+            // 
+            this.btnjugar.AllowDrop = true;
+            this.btnjugar.BackColor = System.Drawing.SystemColors.MenuText;
+            this.btnjugar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnjugar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnjugar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnjugar.Font = new System.Drawing.Font("Felix Titling", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnjugar.ForeColor = System.Drawing.Color.Red;
+            this.btnjugar.Location = new System.Drawing.Point(0, 432);
+            this.btnjugar.Name = "btnjugar";
+            this.btnjugar.Size = new System.Drawing.Size(453, 46);
+            this.btnjugar.TabIndex = 3;
+            this.btnjugar.Text = "JUGAR";
+            this.btnjugar.UseVisualStyleBackColor = false;
+            this.btnjugar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(453, 47);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::FizzBuzz.Properties.Resources.cerrar1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(396, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 41);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(453, 478);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnjugar);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnumero;
+        private System.Windows.Forms.Button btnjugar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FizzBuzz/Form2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FizzBuzz
+{
+    public partial class Form2 : Form
+    {
+        //LblMensaje.Text = texto; //control label
+        int Valor;
+
+        public void metodo()
+        {
+            for (int i = 1; i <= Valor; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    // txtmostrar.Text = "FIZZBUZZ";
+                    txtmostrar.AppendText("fizzbuzz" + Environment.NewLine);
+
+                }
+                else if (i % 3 == 0)
+                {
+                    txtmostrar.AppendText("fiz" + Environment.NewLine);
+                }
+                else if (i % 5 == 0)
+                {
+                    txtmostrar.AppendText("buz" + Environment.NewLine);
+
+                }
+                else
+                {
+                    txtmostrar.AppendText(i.ToString() + Environment.NewLine);
+                }
+            }
+
+        }
+
+        public Form2(int Valor)
+        {
+            InitializeComponent();
+            this.Valor = Valor;
+            metodo();
+
+
+        }
+        
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtmostrar_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            Hide();
+        }
+    }
+}
